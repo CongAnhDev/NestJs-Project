c2f26f122651a264a052bc73d363d22277e42cd7
Le Cong Anh <huuanh952002@gmail.com>
2024-06-01 01:20:35 +0700

    update

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty } from 'class-validator';
 
 //data transfer object
 export class CreateCompanyDto {
-    @IsNotEmpty({ message: 'Name không được để trống aaa' })
+    @IsNotEmpty({ message: 'Name không được để trống ' })
     name: string;
 
     @IsNotEmpty({ message: 'Address không được để trống' })
